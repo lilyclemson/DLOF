bb70142278b2242419b5006c1454854fe047a424
Arya Adesh <aryaadesh.cs20@rvce.edu.in>
2022-05-10 16:25:42 +0530

    KNN code

--- knn.py
+++ knn.py
@@ -1,10 +1,13 @@
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
-samples=Input_data = np.array([[-1, 1], [-2, 2], [-3, 3], [1, 2], [2, 3], [3, 4],[4, 5]])
-class knn:
-    nn= NearestNeighbors(n_neighbors=5, algorithm="kd_tree")
+
+samples= np.array([[-1, 1], [-2, 2], [-3, 3], [1, 2], [2, 3], [3, 4],[4, 5]]) 
+def knn(samples,k=3):
+   
+    nn= NearestNeighbors(n_neighbors=k, algorithm="kd_tree")
     nn.fit(samples)
-    dis, points=nn.kneighbors(nn)
+    dis, points=nn.kneighbors(samples)
     print(dis)
-    
+samples= np.array([[-1, 1], [-2, 2], [-3, 3], [1, 2], [2, 3], [3, 4],[4, 5]]) 
+out=knn(samples,6);
 
