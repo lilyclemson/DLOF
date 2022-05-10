39f28f91a8d69a2708043ea5eebf315d5726d4a8
Arya Adesh <aryaadesh.cs20@rvce.edu.in>
2022-05-10 14:50:38 +0530

    Importing Libraries

---
+++ main_LOF.py
@@ -0,0 +1,8 @@
+
+import pandas as pd
+import numpy as np
+from collections import Counter
+from sklearn.datasets import make_classification
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import LocalOutlierFactor
