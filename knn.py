8476e35e147323673f65a86e8fe96ba97f5aa6df
Arya Adesh <aryaadesh.cs20@rvce.edu.in>
2022-05-11 11:28:45 +0530

    Make syntethic dataset

--- knn.py
+++ knn.py
@@ -8,6 +8,24 @@ def knn(samples,k=3):
     nn.fit(samples)
     dis, points=nn.kneighbors(samples)
     print(dis)
+    print(points)
+# dis is n*k matrix and returns n=number of datapoints and k is the input which represents the distance of 
+# k nearest neighbors in that point
 samples= np.array([[-1, 1], [-2, 2], [-3, 3], [1, 2], [2, 3], [3, 4],[4, 5]]) 
 out=knn(samples,6);
+#always pass k+1 as parameter if k is number of nearest neighbors
+'''
+if k=5, the point itself is considered as a neighbour so we omit the first entry 
+in the matrix dis
+[[0.         1.41421356 2.23606798 2.82842712 3.60555128]
+ [0.         1.41421356 1.41421356 3.         4.12310563]
+ [0.         1.41421356 2.82842712 4.12310563 5.        ]
+ [0.         1.41421356 2.23606798 2.82842712 3.        ]
+ [0.         1.41421356 1.41421356 2.82842712 3.60555128]
+ [0.         1.41421356 1.41421356 2.82842712 5.        ]
+ [0.         1.41421356 2.82842712 4.24264069 6.40312424]]
+
+'''
+
+
 
--- main_LOF.py
+++ main_LOF.py
@@ -6,3 +6,19 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.datasets import make_classification
+
+total_sample=1000
+X, Y = make_classification(n_samples=total_sample, n_features=5, n_informative=5,
+                           n_redundant=0, n_repeated=0, n_classes=2,
+                           n_clusters_per_class=1,
+                           weights=[0.995, 0.005],
+                           class_sep=0.5, random_state=0)
+
+df = pd.DataFrame({'feature1': X[:, 0], 'feature2': X[:, 1],'feature3': X[:, 2],'feature4': X[:, 3],'feature5': X[:, 4],'target': Y})
+count=0
+for x in df.loc[:,"target"]:
+  if x==1:
+    count=count+1
+print(f"Outliers = {count}")
+print(f"normal = {total_sample-count}")
+
